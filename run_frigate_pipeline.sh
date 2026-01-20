b7ea9a0799451010c1a3457bc23dd7f4a94a8095
mac/cli <chengcli@umich.edu>
2026-01-20 11:00:30 -0500

    wip

---
+++ run_frigate_pipeline.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+## STEP A: Generate configuration files for the site and date range
+python generate_config.py ws-site1 \
+    --start-date 2025-11-01 --end-date 2025-11-07 \
+    --nx1 150 --nx2 40 --nx3 40
+
+## STEP B: Generate configuration files for the site and date range
+python prepare_initial_condition.py ws-site1
